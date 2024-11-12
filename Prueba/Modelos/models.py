6c03c3b19339dfc91f359f2a3db7d9d81ee58eb8
Ana05509 <ana.salguero3883@utc.edu.ec>
2024-11-12 11:00:50 -0500

    Prueba

--- Prueba/Modelos/models.py
+++ Prueba/Modelos/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Estudiante (models.Model):
+    cedula=models.CharField(primary_key=True, max_length=10 )
+    nombre=models.CharField (max_length=30, black =False) 
+    apellido=models.TextField ()
+    edad=models.IntegerField()
+    direccion=models.TextField ()
+    telefono=models.CharField(max_length=10, black =False)
+
+def __str__(self):
+    return self.nombre
+class Docente (models.Model):
+    cedula=models.CharField(primary_key=True, max_length=10 )
+    nombre=models.CharField (max_length=30, black =False) 
+    apellido=models.TextField ()
+    edad=models.IntegerField()
+    direccion=models.TextField ()
+    telefono=models.CharField(max_length=10, black =False)
+
+def __str__(self):
+    return self.nombre
+
+class Calificacion (models.Model):
+    cedula_estu= models.ForeignKey(Calificacion,  on_delete=models.RESTRICT)
+    cedula_doc=models.ForeignKey(Calificacion,  on_delete=models.RESTRICT)
+    calificacion=models.FloatField()
+    
+
+
